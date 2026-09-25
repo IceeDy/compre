da09905b3ce798bcc2db207fb3d000ed63eb6c55
IceeDy <137652842+IceeDy@users.noreply.github.com>
2026-09-25 11:55:48 -0300

    refactor(models): register supplier quote request

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,6 +3,7 @@ from .order import Order, OrderItem
 from .product import Product
 from .proposal import Proposal, ProposalItem
 from .quote import Quote, QuoteItem
+from .quote_supplier_request import QuoteSupplierRequest
 from .role import Role
 from .supplier import Supplier
 from .tenant import Tenant
@@ -10,5 +11,5 @@ from .user import User
 
 __all__ = [
     "Customer", "Order", "OrderItem", "Product", "Proposal", "ProposalItem",
-    "Quote", "QuoteItem", "Role", "Supplier", "Tenant", "User",
+    "Quote", "QuoteItem", "QuoteSupplierRequest", "Role", "Supplier", "Tenant", "User",
 ]
